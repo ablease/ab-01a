e1993460fff8d42bcdcc8e4e72fecafbd0d1a337
Alex Blease <alexblease@gmail.com>
2025-04-01 21:33:19 +0100

    WIP -- update adsr params from UI

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -23,6 +23,8 @@ public:
 	void prepareToPlay(double sampleRate, int samplesPerBlock, int outputChannels);
 	void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
 	void pitchWheelMoved(int newPitchWheelValue) override;
+	void updateADSR(const float attack, const float decay, const float sustain, const float release);
+
 
 private:
 	juce::AudioBuffer<float> synthBuffer;
